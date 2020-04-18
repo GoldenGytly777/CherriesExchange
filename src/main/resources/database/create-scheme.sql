b286deac2443f7651f6ff808f0aa7b81cee9c022
Artur Wiśniewski <artwis1998@gmail.com>
2020-04-19 00:25:58 +0200

    Added Entities,files to create sql-schemas

--- src/main/resources/database/create-scheme.sql
+++ src/main/resources/database/create-scheme.sql
@@ -1,27 +1,9 @@
-DROP TABLE IF exists `address`;
+
 DROP TABLE IF exists `wallet`;
+DROP TABLE IF exists `address`;
+DROP TABLE IF exists `transaction_history`;
+DROP TABLE IF exists `currency`;
 DROP TABLE IF exists `user`;
-
-CREATE TABLE `address` (
-	`id` SERIAL,
-    `country` VARCHAR(32) NOT NULL,
-	`city` VARCHAR(32) NOT NULL,
-	`street` VARCHAR(32) NOT NULL,
-	`house_number` VARCHAR(16) NOT NULL,
-	`zip_code` VARCHAR(8) NOT NULL,
-	`created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    primary key(`id`)
-);
-
-CREATE TABLE `wallet` (
-	`id` SERIAL,
-    `currency` bigint unsigned,
-	`amount`  decimal(10,2) default 0,
-	`created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    primary key(`id`)
-);
 
 CREATE TABLE `user` (
 	`id` SERIAL,
@@ -33,12 +15,58 @@ CREATE TABLE `user` (
     `password` varchar(256) NOT NULL,
     `enabled` boolean default false,
     `blocked` boolean default false,
-	`address_id` bigint unsigned,
-	`wallet_id` bigint unsigned unique,
 	`created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     primary key(`id`),
-    unique key(`email`),
-    foreign key(`address_id`) references `address`(`id`),
-    foreign key(`wallet_id`) references `wallet`(`id`)
+    unique key(`email`)
+);
+
+CREATE TABLE `address` (
+	`id` SERIAL,
+    `user_id` bigint unsigned NOT NULL,
+    `country` VARCHAR(32) NOT NULL,
+	`city` VARCHAR(32) NOT NULL,
+	`street` VARCHAR(32) NOT NULL,
+	`house_number` VARCHAR(16) NOT NULL,
+	`zip_code` VARCHAR(8) NOT NULL,
+	`created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    primary key(`id`),
+    foreign key(`user_id`) references `user`(`id`)
+
+
+);
+CREATE TABLE `currency` (
+	`id` SERIAL,
+    `code` bigint not null,
+    `name` varchar(10)
+);
+
+CREATE TABLE `wallet` (
+	`id` SERIAL,
+    `user_id` bigint unsigned NOT NULL,
+    `currency_id` bigint unsigned NOT NULL,
+	`amount`  decimal(10,2) default 0,
+	`created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    primary key(`id`),
+	FOREIGN KEY(`user_id`) REFERENCES `user`(`id`),
+    FOREIGN KEY(`currency_id`) REFERENCES `currency`(`id`)
+);
+
+CREATE TABLE `transaction_history` (
+	`id` SERIAL,
+    `user_id` bigint unsigned NOT NULL,
+    `currency_from` bigint unsigned NOT NULL,
+    `currency_to` bigint unsigned NOT NULL,
+    `value_from` decimal(10,2) default 0,
+    `value_to` decimal(10,2) default 0,
+    `amount_from`  decimal(10,2) default 0,
+    `amount_to`  decimal(10,2) default 0,
+	`created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    primary key(`id`),
+	FOREIGN KEY(`user_id`) REFERENCES `user`(`id`),
+    FOREIGN KEY(`currency_from`) REFERENCES `currency`(`id`),
+    FOREIGN KEY(`currency_to`) REFERENCES `currency`(`id`)
 );
