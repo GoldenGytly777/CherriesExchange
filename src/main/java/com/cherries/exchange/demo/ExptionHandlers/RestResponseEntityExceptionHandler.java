48e6d04be37e827b8bc33b9f5b0c95695a031bb3
Artur Wiśniewski <artwis1998@gmail.com>
2020-05-09 22:26:32 +0200

    Updated exception handler for exception: EntityNotFoundException

--- src/main/java/com/cherries/exchange/demo/ExptionHandlers/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/cherries/exchange/demo/ExptionHandlers/RestResponseEntityExceptionHandler.java
@@ -1,25 +1,35 @@
 package com.cherries.exchange.demo.ExptionHandlers;
 
-import com.cherries.exchange.demo.entities.Address;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
+import java.sql.Timestamp;
 import javax.persistence.EntityNotFoundException;
+import javax.servlet.http.HttpServletRequest;
+
 
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler
         extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value =  EntityNotFoundException.class )
-    protected ResponseEntity<Object> handleNotFound(RuntimeException ex, WebRequest request) {
-        return new ResponseEntity<Object>(
+    protected ResponseEntity<Object> handleNotFound(RuntimeException ex, HttpServletRequest request) {
+        //IETF 7807
+        ErrorResponse errorResponse = new ErrorResponse(
+                new Timestamp(System.currentTimeMillis()).toString(),
+                HttpStatus.NOT_FOUND.value(),
+                EntityNotFoundException.class.getName(),
                 ex.getMessage(),
-                new HttpHeaders(),
+                request.getRequestURI());
+
+        return new ResponseEntity<Object>(
+                errorResponse,
                 HttpStatus.NOT_FOUND);
     }
 }
